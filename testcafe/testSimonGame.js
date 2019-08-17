d36c09008657d25cfd6379ba748ea8cd332153ad
wong.chris <wong.chris@principal.com>
2019-08-17 23:48:12 +0800

    simongame test

--- testcafe/testSimonGame.js
+++ testcafe/testSimonGame.js
@@ -1,10 +1,11 @@
 import { Selector } from 'testcafe';
 
-fixture `New Fixture`
-    .page `https://staticfilecontent.z7.web.core.windows.net`;
+fixture`New Fixture`
+    .page`https://staticfilecontent.z7.web.core.windows.net`;
 
 test('New Test', async t => {
     await t
         .click(Selector('button').withText('Start'))
-        .expect(Selector('span').withText('1')()).eql(1);
+        .expect(Selector('span').withText('1')().textContent).eql('1');
+
 });
