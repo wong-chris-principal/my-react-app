6b066caa1b43d9663904779bc628f8b02afeeb89
wong.chris <wong.chris@principal.com>
2019-08-19 11:08:18 +0800

    A

--- testcafe/test1.js
+++ testcafe/test1.js
@@ -7,5 +7,5 @@ test('New Test', async t => {
     await t
         .typeText(Selector('.new-todo[data-reactid=".0.0.1"]'), 'A')
         .pressKey('enter')
-        .expect(Selector('label').withText('A').textContent).eql("AB");
+        .expect(Selector('label').withText('A').textContent).eql("A");
 });
