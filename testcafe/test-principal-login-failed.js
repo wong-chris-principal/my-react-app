64ad5a75b61847f6c61c601a59529103964b3336
Chris Wong <wong.chris@principal.com>
2019-08-21 01:13:07 +0800

    Update test-principal-login-failed.js

--- testcafe/test-principal-login-failed.js
+++ testcafe/test-principal-login-failed.js
@@ -1,22 +1,22 @@
 import { Selector } from 'testcafe';
 
 fixture `New Fixture`
-    .page `https://www.principal.com.hk/zh`;
+    .page `https://rsc.principal.com.hk/Principal.WebApp/htdocs/initial.aspx?lang=zh-CN`;
 
 test('New Test', async t => {
     await t
-        .click(Selector('li').withText('English').find('.fa.fa-user'))
-        .wait(500)
-        .click(Selector('#LoginId'))
-        .wait(500)
         .click(Selector('#logintype_0'))
         .wait(500)
         .click(Selector('#schemeType_0'))
         .wait(500)
-        .typeText(Selector('#txtHKID'), 'A123456(7)')
+        .typeText(Selector('#LoginId'), 'test')
+        .wait(500)
+        .pressKey('tab')
+        .wait(500)
+        .typeText(Selector('#password'), '12345678')
         .wait(500)
         .click(Selector('#Login'))
         .wait(500)
-        .expect(Selector('#message').find('font')()).eql("尚未輸入密碼，請重新輸入")
-        .wait(1000);
+        .expect(Selector('#message').find('font').innerText).eql("就選擇的計劃所輸入的登入編號或密碼不正確，請重新輸入。")
+        .wait(2000);
 });
