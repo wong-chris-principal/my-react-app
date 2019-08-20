e5b3478f53ce94972e125fe126732ae637580d77
wong.chris <wong.chris@principal.com>
2019-08-20 18:42:28 +0800

    update tests

--- testcafe/testConsole.js
+++ testcafe/testConsole.js
@@ -6,9 +6,11 @@ fixture`New Fixture`
 test('Test - input command', async t => {
     await t
         .click(Selector('div').withAttribute('class', 'commandContainer'))
-        .wait(2000)
-        .pressKey('c space 1 0 space 1 0 enter b space 1 space 1 enter')
-        .wait(2000)
+        .wait(1000)
+        .pressKey('c space 1 0 space 1 0 enter')
+        .wait(1000)
+        .pressKey('b space 1 space 1 space p enter')
+        .wait(1000)
         .expect('1').eql('1');
 
 });
